5b7b6ba3c2923d99533af0637bd2b43b6e9c172c
Андрей Михин <mikhinandrei@gmail.com>
2021-04-20 00:18:35 +0300

    refactor https://github.com/ivanbukhtiyarov/elevators/issues/6

--- src/command_processor.py
+++ src/command_processor.py
@@ -7,14 +7,23 @@ from src.elevator import Elevator
 Command = namedtuple('Command', ['source', 'action', 'value'], defaults=['0'])
 
 
+def handle_move_to_floor(command: Command, elevator: Elevator):
+    try:
+        floor = int(command.value)
+    except ValueError:
+        print('Sorry, bad value. Integer is required')
+        return
+    print(f'Moving to floor {floor}')
+    elevator.current_floor = floor
+
+
 def process(command: Command, elevator: Elevator):
-    if command.action == 'moveToFloor':
-        try:
-            floor = int(command.value)
-        except ValueError:
-            print('Sorry, bad value. Integer is required')
-            return
-        print(f'Moving to floor {floor}')
-        elevator.current_floor = floor
-    else:
+    dispatcher = {
+        'moveToFloor': handle_move_to_floor,
+    }
+        
+    if command.action not in dispatcher:
         print('Sorry, not implemented at the moment')
+        return
+    
+    dispatcher[command.action](command, elevator)
