f1f37498aa9493c91d4d7daaa43a4541d59d74fc
JellyBongo <superseriousbox@gmail.com>
2021-05-27 00:20:46 +0300

    Фикс индексов лифтов

--- src/operator.py
+++ src/operator.py
@@ -20,34 +20,34 @@ class Operator:
         self.elevators_list = elevators
 
     def open_doors(self, num: int) -> None:
-        if num < 1 or num > len(self.elevators_list):
+        if num < 0 or num >= len(self.elevators_list):
             raise ValueError('Wrong elevator number')
-        self.elevators_list[num - 1].open_doors()
+        self.elevators_list[num].open_doors()
 
     def close_doors(self, num: int) -> None:
-        if num < 1 or num > len(self.elevators_list):
+        if num < 0 or num >= len(self.elevators_list):
             raise ValueError('Wrong elevator number')
-        self.elevators_list[num - 1].close_doors()
+        self.elevators_list[num].close_doors()
 
     def call_dispatcher(self, num: int) -> None:
-        if num < 1 or num > len(self.elevators_list):
+        if num < 0 or num >= len(self.elevators_list):
             raise ValueError('Wrong elevator number')
         """
         В доке написан про попытку
         ToDo: Реализовать именно попытку
         """
         try:
-            self.elevators_list[num - 1].call_dispatcher()
+            self.elevators_list[num].call_dispatcher()
         except Exception as e:
             pass
 
     def move_to_floor(self, floor: int, num: int) -> None:
-        if num < 1 or num > len(self.elevators_list):
+        if num < 0 or num >= len(self.elevators_list):
             raise ValueError('Wrong elevator number')
-        self.elevators_list[num - 1].move_to_floor(floor)
+        self.elevators_list[num].move_to_floor(floor)
 
     def get_elevator_state(self, num: int) -> dict:
-        if num < 1 or num > len(self.elevators_list):
+        if num < 0 or num >= len(self.elevators_list):
             raise ValueError('Wrong elevator number')
         """
         Состояние кабины лифта
@@ -55,91 +55,91 @@ class Operator:
         мб в классе лифта собрать инфу со всех датчиков
         """
         state = {
-            "direction": self.elevators_list[num - 1].current_direction,
-            "weight": self.elevators_list[num - 1].current_weight,
-            "light": self.elevators_list[num - 1].is_light_on,
-            "smoke": self.elevators_list[num - 1].is_smoked,
-            "requests": self.elevators_list[num - 1].requests,
-            "communication": self.elevators_list[num - 1].is_communication_on,
-            "is_doors_open": self.elevators_list[num - 1].is_doors_open,
-            "is_empty": self.elevators_list[num - 1].is_empty,
+            "direction": self.elevators_list[num].current_direction,
+            "weight": self.elevators_list[num].current_weight,
+            "light": self.elevators_list[num].is_light_on,
+            "smoke": self.elevators_list[num].is_smoked,
+            "requests": self.elevators_list[num].requests,
+            "communication": self.elevators_list[num].is_communication_on,
+            "is_doors_open": self.elevators_list[num].is_doors_open,
+            "is_empty": self.elevators_list[num].is_empty,
         }
         return state
 
     def restart(self, num: int) -> None:
-        if num < 1 or num > len(self.elevators_list):
+        if num < 0 or num >= len(self.elevators_list):
             raise ValueError('Wrong elevator number')
 
-        self.elevators_list[num - 1].current_floor = 1
-        self.elevators_list[num - 1].current_direction = 0
-        self.elevators_list[num - 1].current_weight = 0
-        self.elevators_list[num - 1].turn_light_on()
-        self.elevators_list[num - 1].is_smoked = False
-        self.elevators_list[num - 1].requests = []
-        self.elevators_list[num - 1].is_doors_blocked = False
-        self.elevators_list[num - 1].is_communication_on = False
-        self.elevators_list[num - 1].open_doors()
-        self.elevators_list[num - 1].is_empty = True
-        self.elevators_list[num - 1].move_to_floor(1)
+        self.elevators_list[num].current_floor = 1
+        self.elevators_list[num].current_direction = 0
+        self.elevators_list[num].current_weight = 0
+        self.elevators_list[num].turn_light_on()
+        self.elevators_list[num].is_smoked = False
+        self.elevators_list[num].requests = []
+        self.elevators_list[num].is_doors_blocked = False
+        self.elevators_list[num].is_communication_on = False
+        self.elevators_list[num].open_doors()
+        self.elevators_list[num].is_empty = True
+        self.elevators_list[num].move_to_floor(1)
         # дожны ли эти функции возвращать информацию об успехе операции или что-то еще?
 
     def get_tonnage(self, elevator_id: int) -> Optional[int]:
-        if 0 < elevator_id <= len(self.elevators_list) :
+        if 0 <= elevator_id < len(self.elevators_list):
             return self.elevators_list[elevator_id].tonnage
         return None
 
     def get_floors_count(self, elevator_id: int) -> Optional[int]:
-        if 0 < elevator_id <= len(self.elevators_list) :
+        if 0 <= elevator_id < len(self.elevators_list):
             return self.elevators_list[elevator_id].floors_count
         return None
 
     def get_current_direction(self, elevator_id: int) -> Optional[int]:
-        if 0 < elevator_id <= len(self.elevators_list) :
+        if 0 <= elevator_id < len(self.elevators_list):
             return self.elevators_list[elevator_id].current_direction
         return None
 
     def get_current_weight(self, elevator_id: int) -> Optional[int]:
-        if 0 < elevator_id <= len(self.elevators_list) :
+        if 0 <= elevator_id < len(self.elevators_list):
             return self.elevators_list[elevator_id].current_weight
         return None
 
     def is_light_on(self, elevator_id: int) -> Optional[bool]:
-        if 0 < elevator_id <= len(self.elevators_list) :
+        if 0 <= elevator_id < len(self.elevators_list):
             return self.elevators_list[elevator_id].is_light_on
         return None
 
     def is_smoked(self, elevator_id: int) -> Optional[bool]:
-        if 0 < elevator_id <= len(self.elevators_list) :
+        if 0 <= elevator_id < len(self.elevators_list):
             return self.elevators_list[elevator_id].is_smoked
         return None
 
     def get_requests(self, elevator_id: int) -> Optional[list[MoveRequest]]:
-        if 0 < elevator_id <= len(self.elevators_list) :
+        if 0 <= elevator_id < len(self.elevators_list):
             return self.elevators_list[elevator_id].requests
         return None
 
     def is_communication_on(self, elevator_id: int) -> Optional[bool]:
-        if 0 < elevator_id <= len(self.elevators_list) :
+        if 0 <= elevator_id < len(self.elevators_list):
             return self.elevators_list[elevator_id].is_communication_on
         return None
 
     def is_doors_open(self, elevator_id: int) -> Optional[bool]:
-        if 0 < elevator_id <= len(self.elevators_list) :
+        if 0 <= elevator_id < len(self.elevators_list):
             return self.elevators_list[elevator_id].is_doors_open
         return None
 
     def is_doors_blocked(self, elevator_id: int) -> Optional[bool]:
-        if 0 < elevator_id <= len(self.elevators_list) :
+        if 0 <= elevator_id < len(self.elevators_list):
             return self.elevators_list[elevator_id].is_doors_blocked
         return None
 
     def is_empty(self, elevator_id: int) -> Optional[bool]:
-        if 0 < elevator_id <= len(self.elevators_list) :
+        if 0 <= elevator_id < len(self.elevators_list):
             return self.elevators_list[elevator_id].is_empty
         return None
 
     def get_current_floor(self, elevator_id: int) -> Optional[int]:
-        if 0 < elevator_id <= len(self.elevators_list) :
+        if 0 <= elevator_id < len(self.elevators_list):
             return self.elevators_list[elevator_id].current_floor
         return None
 
@@ -176,49 +176,49 @@ class Operator:
     def set_direction(self, direction, elevator_id: int):
         if direction not in [-1, 0, 1]:
             raise ValueError('Bad direction')
-        if not 0 < elevator_id <= len(self.elevators_list):
+        if not 0 <= elevator_id < len(self.elevators_list):
             raise ValueError('Bad elevator id')
 
         self.elevators_list[elevator_id].current_direction = direction
 
     def block_door(self, elevator_id: int):
-        if not 0 < elevator_id <= len(self.elevators_list):
+        if not 0 <= elevator_id < len(self.elevators_list):
             raise ValueError('Bad elevator id')
 
         self.elevators_list[elevator_id].is_doors_blocked = True
 
     def unblock_door(self, elevator_id: int):
-        if not 0 < elevator_id <= len(self.elevators_list):
+        if not 0 <= elevator_id < len(self.elevators_list):
             raise ValueError('Bad elevator id')
 
         self.elevators_list[elevator_id].is_doors_blocked = False
 
     def turn_smoke_on(self, elevator_id: int):
-        if not 0 < elevator_id <= len(self.elevators_list):
+        if not 0 <= elevator_id < len(self.elevators_list):
             raise ValueError('Bad elevator id')
 
         self.elevators_list[elevator_id].is_smoked = True
 
     def turn_smoke_off(self, elevator_id: int):
-        if not 0 < elevator_id <= len(self.elevators_list):
+        if not 0 <= elevator_id < len(self.elevators_list):
             raise ValueError('Bad elevator id')
 
         self.elevators_list[elevator_id].is_smoked = False
 
     def turn_light_on(self, elevator_id: int):
-        if not 0 < elevator_id <= len(self.elevators_list):
+        if not 0 <= elevator_id < len(self.elevators_list):
             raise ValueError('Bad elevator id')
 
         self.elevators_list[elevator_id].is_light_on = True
 
     def turn_light_off(self, elevator_id: int):
-        if not 0 < elevator_id <= len(self.elevators_list):
+        if not 0 <= elevator_id < len(self.elevators_list):
             raise ValueError('Bad elevator id')
 
         self.elevators_list[elevator_id].is_light_on = False
 
     def set_weight(self, weight: int, elevator_id: int):
-        if not 0 < elevator_id <= len(self.elevators_list):
+        if not 0 <= elevator_id < len(self.elevators_list):
             raise ValueError('Bad elevator id')
         if weight < 0:
             raise ValueError('Weight cannot be less than 0')
